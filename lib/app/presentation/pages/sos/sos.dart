66a4e2a9ee73401c29e192da19f3b684f8b1d319
Vitoria <vick_santos_vick@hotmail.com>
2023-04-23 17:52:27 -0300

    -- criando páginas vazias home e sos

---
+++ lib/app/presentation/pages/sos/sos.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Sos extends StatefulWidget {
+  const Sos({super.key});
+
+  @override
+  State<Sos> createState() => _SosState();
+}
+
+class _SosState extends State<Sos> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
