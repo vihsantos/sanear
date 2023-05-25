4c37475bdd4a29bb268d1b9aa55b3f6c6d04caa7
Vitória <vick_santos_vick@hotmail.com>
2023-05-24 23:43:36 -0300

    --- montando a page sos

--- lib/app/presentation/pages/sos/sos.dart
+++ lib/app/presentation/pages/sos/sos.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../data/models/sos_model.dart';
+
 class Sos extends StatefulWidget {
   const Sos({super.key});
 
@@ -8,8 +10,61 @@ class Sos extends StatefulWidget {
 }
 
 class _SosState extends State<Sos> {
+  List<SosModel> dados = [
+    SosModel("DEFESA CIVIL",
+        "Quando alagamento, inundaçoes, deslizamento, desmoronamento", "199"),
+    SosModel(
+        "DEFESA CIVIL", "Quando ocorrência de afogamento, soterramento", "199"),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    Size size = MediaQuery.of(context).size;
+    SosModel dropdownValue = dados.first;
+    return Scaffold(
+      backgroundColor: const Color(0xffFAFAFA),
+      appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+          title: const Text(
+            "SOS",
+            style: TextStyle(
+                color: Color(0xff0D1937),
+                fontSize: 36,
+                fontWeight: FontWeight.bold),
+          )),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(5),
+              color: const Color.fromARGB(255, 222, 231, 235),
+              height: size.height * 0.1,
+              width: size.width,
+              child: DropdownButton<SosModel>(
+                padding: const EdgeInsets.all(5),
+                isExpanded: true,
+                value: dropdownValue,
+                elevation: 0,
+                onChanged: (SosModel? value) {
+                  setState(() {
+                    dropdownValue = value!;
+                  });
+                },
+                items: dados.map<DropdownMenuItem<SosModel>>((SosModel value) {
+                  return DropdownMenuItem<SosModel>(
+                    value: value,
+                    child: Text(value.situacao),
+                  );
+                }).toList(),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
