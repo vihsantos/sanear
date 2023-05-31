472f2d52206196bf0a551e98f57283558ce533e8
Vitória <vick_santos_vick@hotmail.com>
2023-05-31 20:53:21 -0300

    --- adicionando cores

--- lib/app/presentation/utils/color_palette.dart
+++ lib/app/presentation/utils/color_palette.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ColorPalette {
-  static const Color diasdesol_01 = Color(0xFFFDF2D7);
+  static const Color previsaosol_01 = Color(0xFFFDF2D7);
   static const Color diasdesol_02 = Color(0xFFE97E31);
+  static const Color labelcolor = Color(0xFF7A7A7A);
+  static const Color backgroundcolor = Color(0xFFFAFAFA);
+  static const Color backgroundsearch = Color(0xFFEBF4FA);
 }
