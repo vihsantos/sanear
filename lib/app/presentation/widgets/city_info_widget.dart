6068b4f58a0b30023db6c950471a02b3de1f490e
reinans <reinandesouza01@gmail.com>
2023-06-15 22:33:57 -0300

    habilita permissoes de internet

--- lib/app/presentation/widgets/city_info_widget.dart
+++ lib/app/presentation/widgets/city_info_widget.dart
@@ -90,62 +90,6 @@ class CityInfoWidget extends StatelessWidget {
             ],
           ),
         ),
-        Column(children: [
-          Container(
-            decoration: BoxDecoration(
-              color: const Color(0xFFFDF2D7),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            margin: const EdgeInsets.all(15),
-            padding: const EdgeInsets.all(15),
-            width: size.width,
-            height: size.height * 0.26,
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Image(
-                        image: NetworkImage(
-                          Urls.weatherIcon(weather.current.condition.icon),
-                          scale: 0.7,
-                        ),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              Icon(Icons.pin_drop_outlined),
-                              SizedBox(width: 15),
-                              Text(weather.location.name)
-                            ],
-                          ),
-                          Text(
-                            weather.current.condition.text,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 35),
-                  Text("Temperatura"),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                            "${weather.forecast.forecastday[0].day.maxtempC}º MAX"),
-                        Text(
-                            "${weather.forecast.forecastday[0].day.mintempC}º MIN"),
-                      ]),
-                ]),
-          ),
-        ]),
       ],
     );
   }
